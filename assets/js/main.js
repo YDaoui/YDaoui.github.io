6e33081fbcd5f0d9d03e92b231645156dffd8623
Yassine Daoui <daoui00yassine@gmail.com>
2025-04-23 15:22:58 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,17 @@
-/* MENU SHOW */ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// main.js
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const navbar = document.querySelector('.navbar');
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
+  // Quand on survole l'icône hamburger, on ouvre le menu et on applique la rotation
+  hamburger.addEventListener('mouseenter', () => {
+    navbar.classList.add('active');
+    hamburger.classList.add('rotate-icon');
+  });
 
-showMenu('nav-toggle','nav-menu')
-
-/*----- ANIMATE -----*/
-// OVERLAY
-gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
-
-// IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
-
-// INFORMATION
-gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
-gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
-
-// NAV ITEM
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
-
-// SOCIAL
-gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+  // Quand on sort du menu latéral, on le referme et on remet l'icône à sa position initiale
+  navbar.addEventListener('mouseleave', () => {
+    navbar.classList.remove('active');
+    hamburger.classList.remove('rotate-icon');
+  });
+});
