cc8d69ef0a54cf448f1703726c396d356b5b94d0
Yassine Daoui <daoui00yassine@gmail.com>
2025-05-01 16:47:59 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM fully loaded");
 
     /* ===== MENU TOGGLE ===== */
